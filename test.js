6d8effff7e63a2416e522af5664e6b424a8e8d8e
astur <astur@yandex.ru>
2017-05-01 23:39:07 +0300

    no more url parts in options

--- test.js
+++ test.js
@@ -1,10 +1,6 @@
 const scra = require('.');
 
 describe('GET', () => {
-    it('should perform request with easy options object', (done) => {
-        scra({hostname: 'httpbin.org', path: '/get'})
-            .then((res)=>done(), (err)=>done('false eror is raised'));
-    });
     it('should perform request with easy string url', (done) => {
         scra('http://httpbin.org/get')
             .then((res)=>done(), (err)=>done('false eror is raised'));
