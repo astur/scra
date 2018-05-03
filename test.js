20d6b45038fa159ccca9e27486af543208670a0b
astur <astur@yandex.ru>
2018-05-03 23:03:10 +0300

    typo fixed

--- test.js
+++ test.js
@@ -5,15 +5,15 @@ const scra = require('.');
 describe('GET', () => {
     it('should perform request with easy string url', done => {
         scra('http://httpbin.org/get')
-            .then(res => done(), err => done('false eror is raised'));
+            .then(res => done(), err => done('false error is raised'));
     });
     it('should perform request url w/o protocol', done => {
         scra('httpbin.org/get')
-            .then(res => done(), err => done('false eror is raised'));
+            .then(res => done(), err => done('false error is raised'));
     });
     it('should perform request with url in object field', done => {
         scra({url: 'http://httpbin.org/get'})
-            .then(res => done(), err => done('false eror is raised'));
+            .then(res => done(), err => done('false error is raised'));
     });
 });
 
@@ -22,14 +22,14 @@ describe('POST', () => {
         scra({url: 'http://httpbin.org/post', data: 'a=1&b=2'})
             .then(
                 res => done(res.body.form.a === '1' && res.body.form.b === '2' ? undefined : 'post error'),
-                err => done('false eror is raised')
+                err => done('false error is raised')
             );
     });
     it('should perform post request with plain text', done => {
         scra({url: 'http://httpbin.org/post', data: 'test', headers: {'Content-Type': 'plain/text'}})
             .then(
                 res => done(res.body.data === 'test' ? undefined : 'post error'),
-                err => done('false eror is raised')
+                err => done('false error is raised')
             );
     });
 });
@@ -38,20 +38,20 @@ describe('JSON', () => {
     it('should parse JSON response to object', done => {
         scra({url: 'http://httpbin.org/user-agent'}).then(
             res => done(res.body['user-agent'] === 'astur/scra' ? undefined : 'json parse error'),
-            err => done('false eror is raised')
+            err => done('false error is raised')
         );
     });
     it('should not parse HTML response to object', done => {
         scra({url: 'http://httpbin.org/html'}).then(
             res => done(typeof res.body === 'string' ? undefined : 'unnecessary json parse error'),
-            err => done('false eror is raised')
+            err => done('false error is raised')
         );
     });
     it('should perform post request with json', done => {
         scra({url: 'http://httpbin.org/post', data: {a: 1, b: 2}})
             .then(
                 res => done(res.body.json.a === 1 && res.body.json.b === 2 ? undefined : 'post error'),
-                err => done('false eror is raised')
+                err => done('false error is raised')
             );
     });
 });
@@ -60,13 +60,13 @@ describe('Compression', () => {
     it('should decode Gzip-compressed response', done => {
         scra({url: 'http://httpbin.org/gzip'}).then(
             res => done(res.body.gzipped ? undefined : 'gzip error'),
-            err => done('false eror is raised')
+            err => done('false error is raised')
         );
     });
     it('should decode Deflate-compressed response', done => {
         scra({url: 'http://httpbin.org/deflate'}).then(
             res => done(res.body.deflated ? undefined : 'gzip error'),
-            err => done('false eror is raised')
+            err => done('false error is raised')
         );
     });
 });
@@ -75,13 +75,13 @@ describe('Cookies', () => {
     it('should send cookies from options', done => {
         scra({url: 'http://httpbin.org/cookies', cookies: {a: 1}}).then(
             res => done(res.body.cookies.a === '1' ? undefined : 'cookies error'),
-            err => done('false eror is raised')
+            err => done('false error is raised')
         );
     });
     it('should store cookies in response', done => {
         scra({url: 'http://httpbin.org/cookies/set?a=1'}).then(
             res => done(res.cookies.a === '1' ? undefined : 'cookies error'),
-            err => done('false eror is raised')
+            err => done('false error is raised')
         );
     });
 });
